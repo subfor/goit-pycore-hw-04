acb25355b4180c3a6388ddde461355e22b25fbb7
alex <alex.kudelko@gmail.com>
2025-03-17 11:03:34 +0200

    task 4

--- task_4.py
+++ task_4.py
@@ -1,16 +1,57 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+
+def change_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact updated."
+
+
+def show_phone(args, contacts):
+    name = args[0]
+    return f"Phone number: {contacts.get(name)}"
+
+
+def show_all(contacts):
+    return contacts
+
+
 def main():
+    contacts = {}
     print("Welcome to the assistant bot!")
     while True:
-        command = input("Enter a command: ").strip().lower()
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-
-        elif command == "hello":
-            print("How can I help you?")
-        else:
-            print("Invalid command.")
+        match command:
+            case "close" | "exit":
+                print("Good bye!")
+                break
+            case "hello":
+                print("How can I help you?")
+            case "add":
+                print(add_contact(args, contacts))
+            case "all":
+                if not contacts:
+                    print("Contacts not found.")
+                all_contacts = show_all(contacts)
+                for name, number in all_contacts.items():
+                    print(f"Name: {name}, Phone number: {number}")
+            case "change":
+                print(change_contact(args, contacts))
+            case "phone":
+                print(show_phone(args, contacts))
+            case _:
+                print("Invalid command.")
 
 
 if __name__ == "__main__":
